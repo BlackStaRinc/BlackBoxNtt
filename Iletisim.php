ab5b753e694dc46edce43a703d382fb1e69da91f
BlackStar <timatibaba@gmail.com>
2016-05-06 21:53:41 +0300

    İçerik Ekleme

--- Iletisim.php
+++ Iletisim.php
@@ -182,7 +182,7 @@
         <div class="Cell">
         </div> 
         <div class="Cell">
-        <input type="submit">
+        <input type="submit" value="Gönder">
         </div>
         </div>
         
@@ -198,19 +198,7 @@
         </div>
     	</div>
     
-</form>
-       <div id="fb-root"></div>
-            <script>(function(d, s, id) {
-              var js, fjs = d.getElementsByTagName(s)[0];
-              if (d.getElementById(id)) return;
-              js = d.createElement(s); js.id = id;
-              js.src = "//connect.facebook.net/tr_TR/sdk.js#xfbml=1&version=v2.6";
-              fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));</script>
-            
-            <div class="fb-page" data-href="https://www.facebook.com/facebook" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
-            <div class="fb-xfbml-parse-ignore">
-    <blockquote cite="https://www.facebook.com/facebook"><a href="https://www.facebook.com/facebook">Facebook</a></blockquote></div>																													   	</div>     
+</form> 
 	</div>
 	</div>
 </body>
